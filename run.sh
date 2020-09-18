390faed5cb9805b4ab756acc50672f23dc1926d1
Marc12O <obfuscator.12@gmail.com>
2020-09-18 14:55:16 +0200

    added -d

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -it --name sdrplay_dump1090 -p 8080:8080 -p 30002:30002 -p 30005:30005 --device /dev/bus/usb f4fhh/sdrplay_dump1090
+docker run -d --name sdrplay_dump1090 -p 8080:8080 -p 30002:30002 -p 30005:30005 --device /dev/bus/usb f4fhh/sdrplay_dump1090
